295f097bdc4a21c71e363b1f1e858b5123a4d583
QuqqU <rldnd913@naver.com>
2021-01-10 02:01:54 +0900

    페이징 / pylint

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -10,12 +10,14 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list')
 def _list():
+    page = request.args.get('page', type=int, default=1)
     question_list = Question.query.order_by(Question.create_day.desc())
+    question_list = question_list.paginate(page, per_page=10)
     return render_template('question/question_list.html',
                            question_list=question_list)
 
 
-@bp.route('/detail/<int:question_id>/')
+@ bp.route('/detail/<int:question_id>/')
 def detail(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
@@ -24,7 +26,7 @@ def detail(question_id):
                            form=form)
 
 
-@bp.route('/create', methods=('get', 'post'))
+@ bp.route('/create', methods=('get', 'post'))
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
